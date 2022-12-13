f71dc1f2bcb7eaef884ee056334bfa64ae156c5e
MotaNT <natasha.silva4@fatec.sp.gov.br>
2022-12-13 01:08:23 -0300

    Implementado model de produto. closes #2

---
+++ src/main/java/br/com/newgo/mercado/models/Produto.java
@@ -0,0 +1,86 @@
+package br.com.newgo.mercado.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "produtos")
+public class Produto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String descricao;
+    private String imagemCaminho;
+
+    public Produto() {
+    }
+
+    public Produto(UUID id, String nome, String descricao, String imagemCaminho) {
+        setId(id);
+        setNome(nome);
+        setDescricao(descricao);
+        setImagemCaminho(imagemCaminho);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getImagemCaminho() {
+        return imagemCaminho;
+    }
+
+    public void setImagemCaminho(String imagemCaminho) {
+        this.imagemCaminho = imagemCaminho;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", imagemCaminho='" + imagemCaminho + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return id.equals(produto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
