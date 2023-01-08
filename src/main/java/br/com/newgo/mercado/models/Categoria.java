207771e0444cac6ef3dda9942ebe5671960a7d08
MotaNT <natasha.silva4@fatec.sp.gov.br>
2023-01-08 11:23:48 -0300

    feat: criado entidade categoria. closes #13

---
+++ src/main/java/br/com/newgo/mercado/models/Categoria.java
@@ -0,0 +1,20 @@
+package br.com.newgo.mercado.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "CATEGORIAS")
+public class Categoria extends AbstractEntity{
+
+    private String nome;
+
+}
